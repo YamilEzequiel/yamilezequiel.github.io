1ea1c3b83027b59520413f80e301fa728b76a7a1
Yamil <yamillazzari@gmail.com>
2020-12-06 14:46:16 -0300

    CV - Yamil Ezequiel

---
+++ index.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV - Yamil Ruscalleda</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+
+                <div class="card shadow text-center" style="width: 16rem;">
+                    <img src="photo.jpg" class="card-img-top shadow" alt="Foto de perfil">
+                    <div class="card-body">
+                        <h5 class="btn btn-primary">Yamil Ruscalleda</h5>
+                        <p class="card-text"><strong>Cuil:</strong> 23-37715139-9</p>
+                        <p class="card-text"><strong>Dni:</strong> 37715139</p>
+                        <p class="card-text">16 de Mayo 1994</p>
+                    </div>
+                </div>
+                <br>
+                <div class="card shadow text-center" style="width: 16rem;">
+                    <div class="card-body">
+                        <a href="http://198.50.154.144/~yamilruscalleda/CV/" target="_black"> <button class="btn btn-success">CV Online</button></a>
+                    </div>
+                </div>
+                <br>
+                <div class="card shadow text-center" style="width: 16rem;">
+                    <div class="card-body">
+                        <a href="https://www.google.com/maps/place/Rosario,+Santa+Fe/" target="_blank" class="btn btn-outline-info">Argentina - Rosario</a><br><br>
+                        <a href="https://api.whatsapp.com/send?phone=3413203877" target="_blank" class="btn btn-outline-success">(341) 153203877</a><br><br>
+                        <a href="mailto:yamillazzari@gmail.com" target="_blank" class="btn btn-outline-danger">Yamillazzari@gmail.com</a><br>
+                    </div>
+                </div>
+                <br>
+                <div class="card shadow text-center" style="width: 16rem;">
+                    <div class="card-body">
+                        <p class="card-text"><strong>Estado Civil:</strong> Concubino</p>
+                        <p class="card-text"><strong>Disponibilidad:</strong> Full-Time</p>
+                        <p class="card-text"><strong>Carnet de conducir:</strong> Clase B</p>
+                    </div>
+                </div>
+                <br>
+                <div class="card shadow text-center" style="width: 16rem;">
+                    <div class="card-body">
+                        <p class="btn btn-secondary"><strong>Idiomas</strong></p>
+                        <p class="card-text"><strong>Ingles Oral:</strong> Basico</p>
+                        <p class="card-text"><strong>Ingles Escrito:</strong> Intermedio</p>
+                        <p class="card-text"><strong>Español:</strong> Nativo</p>
+                    </div>
+                </div>
+                <br>
+                <div class="card shadow text-center" style="width: 16rem;">
+                    <div class="card-body">
+                        <a href="https://github.com/YamilEzequiel" target="_blank"><img src="image/github.png"></a>
+                        <!-- <a href="https://www.facebook.com/YamilLazzari/" target="_blank"><img src="image/facebook.png" ></a>-->
+                        <a href="https://www.linkedin.com/in/yamil-ruscalleda-619a1915b/" target="_blank"><img src="image/linkedin.png"></a>
+                        <a href="https://es.stackoverflow.com/users/58438/yamil-lazzari" target="_black"><img src="image/stackoverflow.png"></a>
+                    </div>
+                </div> <br>
+                <div class="card shadow text-center" style="width: 16rem;">
+                    <div class="card-body">
+                        <p class="btn btn-secondary"><strong>CV</strong></p>
+                        <p class="card-text">Realizado con HTML5 + Bootstrap</p>
+                    </div>
+                </div>
+                <br>
+                <div class="card shadow text-center" style="width: 16rem;">
+                    <div class="card-body">
+                        <button class="btn btn-light" id="btn_white">White</button>
+                        <button class="btn btn-dark" id="btn_dark">Dark</button>
+                    </div>
+                </div>
+
+            </div>
+            <!--Finish Col-->
+
+            <div class="col-sm-9 ">
+
+                <div class="card shadow">
+                    <div class="card-body">
+                        <p class="btn btn-secondary"><strong>Experiencia Laboral</strong></p><br>
+
+                        <ul>
+                            <li>
+                                <strong>2017-Actual</strong><br>
+                                Programador freelance
+                            </li><br>
+
+                            <li>
+                                <strong>2016-Actual</strong><br>
+                                Tecnico Revisador telefónico/internet de Telecom Argentina - Rosario
+                            </li><br>
+
+                            <li>
+                                <strong>2013-2016</strong><br>
+                                Repositor, preparación de pedidos, carga y descarga
+                                de vehículos. Distribuidora Pampa SRL Av Uriburu 2345 Ref: (0341)6181468
+                            </li><br>
+
+                            <li>
+                                <strong>2012-2013</strong><br>
+                                Ayudante soldador y montaje estructurales
+                                en galpones, albañilería en general, instalaciones electricas
+                                e instalaciones de durlock . Ref: (0341)5144199
+                            </li><br>
+
+
+                            <li>
+                                <strong>2010-2012 </strong><br>
+                                Taller Mecánico G.O.L Servicios, Ref: (0341)5018664
+                            </li>
+
+                        </ul>
+                    </div>
+                </div><br>
+
+                <div class="card shadow">
+                    <div class="card-body">
+                        <p class="btn btn-secondary"><strong>Formación Académica</strong></p><br>
+
+                        <ul>
+                            <li>
+                                <strong> 2007-2011</strong><br>
+                                Polimodal modalidad Económica y Gestión de las
+                                Organizaciones Esc. Media Part Leonardo Da Vinci - Rosario
+                            </li><br>
+
+                            <li>
+                                <strong>2012</strong><br>
+                                Mecánica Automotriz – Cursado y aprobado 1º Año
+                                ITA – Inst. Tecnológico Avanzado – Rosario
+                                <br>
+                                Electricidad automotriz – Finalizado y Aprobado .
+                                ITA – Inst. Tecnológico Avanzado – Rosario.
+                            </li><br>
+
+
+                            <li>
+                                <strong>2014 </strong><br>
+                                Electricidad básica y Revisor telefónico Sitratel – Telecom
+                            </li><br>
+
+                            <li>
+                                <strong>2018 </strong><br>
+                                Curso Android Studio - Instituto IT Rosario
+                            </li><br>
+
+                            <li>
+                                <strong>2009-Actual </strong><br>
+                                Cursos Online y documentaciones de sitios oficiales sobre programación, aprendizaje
+                                autodidacta. Principalmente en lenguajes como Java,C#,PHP utilizando programas
+                                como AndroidStudio,Unity y gestion de base de datos MySql.
+                            </li><br>
+
+                            <li>
+                                <strong>2019</strong><br>
+                                <a href="https://www.udemy.com/course/master-en-api-restful-con-php-laravel-codeigniter/" target="_black">Máster en API RESTful con PHP 7+, Laravel 6</a><br>
+                                Seguridad Informatica y Configuracion de Redes<br>
+                                <a href="https://www.udemy.com/course/master-programacion-de-videojuegos-con-unity-5-y-csharp/" target="_black">Máster en Programación de Videojuegos con Unity® 2019 y C#</a><br>
+                                <a href="https://www.youtube.com/watch?v=kceWXMaJsew&list=PLINy58Bvq5_L2WfuQeZ4t77EEhj-sG675" target="_black">Subneteo de redes</a><br>
+                            </li><br>
+
+                            <li>
+                                <strong>2020</strong><br>
+                                <a href=" https://www.udemy.com/course/api-restful-con-laravel-php-homestead-passport" target="_blanck"> RestFull con Laravel 7 PHP y MySql</a><br>
+                                Kotlin + Firebase + Google<br>
+                                Curso de ingles Basico/Intermedio Online<br>
+                                <a href="https://www.udemy.com/course/javascript-moderno-para-principiantes" target="_black">JavaScript Moderno 2020</a><br>
+                            </li><br>
+
+                        </ul>
+                    </div>
+                </div><br>
+
+                <div class="card shadow">
+                    <div class="card-body">
+                        <p class="btn btn-secondary"><strong>Conocimientos Informáticos</strong></p><br>
+
+                        <ul>
+                            <li>
+                                <strong> Lenguajes de programación:</strong><br>
+                                Php, Java, C#, JavaScript, Android+=(Java,Kotlin).
+                            </li><br>
+
+                            <li>
+                                <strong>Etiquetas y diseño web:</strong><br>
+                                Html5, Css3 ,Bootstrap, Tailwind.
+                            </li><br>
+
+                            <li>
+                                <strong>Base de datos:</strong><br>
+                                MariaDb, Mysql, PhpMyAdmin, WHM.
+                            </li><br>
+
+                            <li>
+                                <strong>Otros , Framework: </strong><br>
+                                jQuery, AJAX, JSON. Composer, Npm, Node, Laravel-JetStream-Livewire , WP, MyBB, VUE, Drupal.
+                            </li><br>
+
+                            <li>
+                                <strong>Programas: </strong><br>
+                                Eclipse, Android Studio, Dreamweaver, Photoshop, PhotoScape,Acquia,
+                                FlashFXP, PsMonitor, SublimeText, Unity5, Cinema4D, GameMarker, Visual
+                                Studio Code, Git Bash, XAMPP, Microsoft Office, Filezzila , Blender, GitHub.
+                            </li><br>
+
+                            <li>
+                                <strong>Servicios Web: </strong><br>
+                                Redes, Google ( AdSense , AdMob ,AdWords,Firebase, Analytics, Tools, Docs, PlayConsole,
+                                PlayStore, Posicionamiento (ASO y SEO) , MarketingWeb), GitHub, Trello, Diagrams.
+                            </li><br>
+
+                        </ul>
+                    </div>
+                </div><br><br>
+
+            </div>
+            <!--Finish Col-->
+        </div>
+        <!--Finish Row-->
+    </div>
+    <!--Finish Container-->
+
+</body>
+<footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="js/data.js"></script>
+</footer>
+
+</html>
